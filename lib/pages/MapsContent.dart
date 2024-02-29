fa7bda8df1a97e93d0954b306194d53d58169f0b
Ayush kumar <ayushverma8900@gmail.com>
2024-02-29 22:53:23 +0530

    map added

--- lib/pages/MapsContent.dart
+++ lib/pages/MapsContent.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart' as latLng;
 
 class MapsContent extends StatefulWidget {
   const MapsContent({super.key});
@@ -9,14 +11,22 @@ class MapsContent extends StatefulWidget {
 }
 
 class _MapsContentState extends State<MapsContent> {
-  static const LatLng _pGooglePlex = LatLng(37.4223, -122.0848);
+  // static const LatLng _pGooglePlex = LatLng(37.4223, -122.0848);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        initialCameraPosition: CameraPosition(target: _pGooglePlex, zoom: 13),
+        body: FlutterMap(
+      options: MapOptions(
+        initialCenter: latLng.LatLng(22.253455, 84.9139524),
+        initialZoom: 15,
       ),
-    );
+      children: [
+        TileLayer(
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          // userAgentPackageName: 'com.example.app',
+        ),
+      ],
+    ));
   }
 }
 
